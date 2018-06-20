b4ae0512c5113ded4b3acccfb020ca641daf4a23
UnderstandF <35169622+UnderstandF@users.noreply.github.com>
2018-06-20 19:53:22 +0000

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,12 +1,5 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const low = require('lowdb')
-const FileSync = require('lowdb/adaptater/FileSync')
-
-const adapter = new FilesSync('database.json');
-const db = low(adapter);
-
-db.defaults({ histoires: [], xp: []}).write()
 
 var prefix = ("&")
 
@@ -19,47 +12,33 @@ bot.login(process.env.TOKEN)
 
 
 bot.on('message', message => {
-    if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n - &help");
+      
+    var msgauthor = message.author.id; 
+              
+    if(message.author.bot)return; 
+                 
+    if(!db.get("xp").find({user: msgauthor}).value()){ 
+            db.get("xp").push({user: msgauthor, xp: 1}).write(); 
+    }else{ 
+        var userxpdb = db.get("xp").filter({user: msgauthor}).find('exp').value(); 
+        console.log(userxpdb); 
+        var userxp = Object.values(userxpdb) 
+        console.log(userxp) 
+        console.log(`Nombre d'xp: ${userxp[1]}`) 
+                    
+        db.get("exp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write; 
+                     
+    if (message.content === prefix + "xp"{ 
+        var xp = db.get("xp").filter({user: msgauthor}).find('xp').value() 
+        var xpfinal = Object.values(xp); 
+        var xp_embed = new Discord.RichEmbed() 
+            .setTitle(`Levels de ${message.author.username}`) 
+            .setColor('#F4D03F') 
+            .setDescription("Affichage des Levels") 
+            .addField("XP:", `${xpfinal[1]} xp`) 
+            .setFooter("Enjoy :)" 
+        message.channel.channel.send({embed: xp_embed});   
+                      
+}}}) 
         
-        var msgauthor = message.author.id;
-    
-        if(message.author.bot)return;
-        
-        if(!db.get("xp").find({user: msgauthor}).value()){
-            db.get("xp").push({user: msgauthor, xp: 1}).write();
-        }else{
-            var userxpdb = db.get("xp").filter({user: msgauthor}).find('exp').value();
-            console.log(userxpdb);
-            var userxp = Object.values(userxpdb)
-            console.log(userxp)
-            console.log(`Nombre d'xp: ${userxp[1]}`)
-            
-            db.get("exp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write;
-            
-    if (message.content === prefix + "xp"{
-        var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
-        var xpfinal = Object.values(xp);
-        var xp_embed = new Discord.RichEmbed()
-            .setTitle(`Levels de ${message.author.username}`)
-            .setColor('#F4D03F')
-            .setDescription("Affichage des Levels")
-            .addField("XP:", `${xpfinal[1]} xp`)
-            .setFooter("Enjoy :)"
-       message.channel.send({embed: xp_embed});  
-            
-}}})
-     
 
-    if (message.content === "Salut"){
-        message.reply("Salut mon pote ! ");
-        console.log("Commande Salut effectué");
-    }
-});
-       
-
-        
-        
-        
-        
-  
